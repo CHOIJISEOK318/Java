5122898c53fda15b6ba2e23b693e62670b740d41
CHOIJISEOK318 <73284183+CHOIJISEOK318@users.noreply.github.com>
2022-06-19 03:29:53 +0900

    Callable and Future

---
+++ java8to11/src/main/java/me/shiteship/java8to11/concurrent/callable_and_future/App.java
@@ -0,0 +1,33 @@
+package me.shiteship.java8to11.concurrent.callable_and_future;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class App {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        Callable<String> hello = () -> {
+            Thread.sleep(2000L);
+            return "Hello";
+        };
+
+        Callable<String> java = () -> {
+            Thread.sleep(3000L);
+            return "Java";
+        };
+
+        Callable<String> keesun = () -> {
+            Thread.sleep(1000L);
+            return "Keesun";
+        };
+
+        String s = executorService.invokeAny(Arrays.asList(hello, java, keesun));
+        System.out.println("s = " + s);
+
+        executorService.shutdown();
+    }
+
+}
