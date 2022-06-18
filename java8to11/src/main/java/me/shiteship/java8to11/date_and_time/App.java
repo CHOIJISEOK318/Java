f3d77de5b77c9fb9e7c61be7ce52ac8a7d94b40b
CHOIJISEOK318 <73284183+CHOIJISEOK318@users.noreply.github.com>
2022-06-19 02:41:22 +0900

    Date, Time API

---
+++ java8to11/src/main/java/me/shiteship/java8to11/date_and_time/App.java
@@ -0,0 +1,62 @@
+package me.shiteship.java8to11.date_and_time;
+
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class App {
+
+    public static void main(String[] args) {
+//        Date date = new Date();
+//        Calendar calendar = new GregorianCalendar();
+//        SimpleDateFormat dateFormat = new SimpleDateFormat();
+
+//         long time = date.getTime();
+//         Calendar keesunBirthDay = new GregorianCalendar(1982,Calendar.JULY,8);
+//         keesunBirthDay.add(Calendar.DAY_OF_YEAR,1);
+//        System.out.println(keesunBirthDay.getTime());
+//        System.out.println(time);
+
+//        Instant instant = Instant.now();
+//        System.out.println("instant = " + instant);
+//        System.out.println(instant.atZone(ZoneId.of("UTC")));
+//        System.out.println(instant.atZone(ZoneId.of("GMT")));
+//
+//        ZoneId zone = ZoneId.systemDefault();
+//        System.out.println("zone = " + zone);
+//
+//        ZonedDateTime zonedDateTime = instant.atZone(zone);
+//        System.out.println("zonedDateTime = " + zonedDateTime);
+
+//        LocalDate now = LocalDate.now();
+//        System.out.println("now = " + now);
+//        LocalDate birthDay = LocalDate.of(1982, Month.JANUARY,15);
+//
+//        ZonedDateTime nowInKorea = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+//        System.out.println("nowInKorea = " + nowInKorea);
+//
+//        System.out.println(nowInKorea.toInstant().atZone(ZoneId.systemDefault()));
+//
+//        Period period = Period.between(now, birthDay);
+//        System.out.println("period = " + period);
+//
+//        Instant instant = Instant.now();
+//        Instant plus = Instant.from(now.plus(10, ChronoUnit.SECONDS));
+//        Duration between = Duration.between(now,plus);
+//        System.out.println("between = " + between);
+
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+//        System.out.println(now.format(dateTimeFormatter));
+//
+//        LocalDate parse = LocalDate.parse("07/15/1982", dateTimeFormatter);
+//        System.out.println("parse = " + parse);
+
+    }
+
+}
